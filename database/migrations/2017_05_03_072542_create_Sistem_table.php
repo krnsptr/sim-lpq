40d2a2c6c832e6cda9d32426953fc053d71bfc87
Kurnia Saputra <krnsptr@gmail.com>
2017-05-10 09:28:12 +0700

    * datatabase/migrations

--- database/migrations/2017_05_03_072542_create_Sistem_table.php
+++ database/migrations/2017_05_03_072542_create_Sistem_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJenisSistemTablee extends Migration
+class CreateSistemTable extends Migration
 {
 
     public function up()
