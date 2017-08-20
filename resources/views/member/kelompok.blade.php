83640004af64a25d05e8bd05bc5d07e9597072c1
Kurnia Saputra <krnsptr@gmail.com>
2017-08-20 17:50:40 +0700

    * view kelompok dirapikan

--- resources/views/member/kelompok.blade.php
+++ resources/views/member/kelompok.blade.php
@@ -35,15 +35,15 @@
         @foreach ($daftar_pengajar as $pengajar)
         <div class="box">
           <div class="box-header with-border">
-            <h3 class="box-title">Pengajar {{$pengajar->jenjang->nama}}</h3>
+            <h3 class="box-title">Pengajar {{$pengajar->jenjang->jenis_program->nama}}</h3>
           </div>
 
           @foreach ($pengajar->daftar_kelompok as $kelompok)
-          <div class="box-body">
+          <div class="box-body table-responsive">
             <table class="table">
             <thead>
               <tr>
-                <td><strong>Kelompok {{$kelompok->id}}</strong></td>
+                <td colspan="2"><strong>Kelompok {{$kelompok->id}}</strong></td>
               </tr>
             </thead>
             <tbody>
@@ -53,25 +53,25 @@
               </tr>
               <tr>
                 <td>Jadwal</td>
-                <td>: {{$hari[$kelompok->jadwal->hari]}}, {{$kelompok->jadwal->waktu}} WIB</td>
+                <td>: {{$hari[$kelompok->jadwal->hari]}}, {{$kelompok->jadwal->waktu}}</td>
               </tr>
             </tbody>
             </table>
 
-            <table class="table">
+            <table class="table table-bordered table-striped">
               <thead>
                 <tr>
-                  <th>Nomor</th>
+                  <th>No.</th>
                   <th>Nama Santri</th>
-                  <th>Nomor HP/WA</th>
+                  <th>Nomor HP, WA</th>
                 </tr>
               </thead>
               <tbody>
                 @foreach ($kelompok->daftar_santri as $santri)
                 <tr>
-                  <td>{{$loop->iteration}}</td>
+                  <td width="5%">{{$loop->iteration}}</td>
                   <td>{{$santri->pengguna->nama_lengkap}}</td>
-                  <td>{{$santri->pengguna->nomor_hp}}</td>
+                  <td>{{$santri->pengguna->nomor_hp}}, {{$santri->pengguna->nomor_wa}}</td>
                 </tr>
                 @endforeach
               </tbody>
@@ -92,16 +92,16 @@
         @foreach ($daftar_santri as $santri)
         <div class="box">
           <div class="box-header with-border">
-            <h3 class="box-title">Santri {{$santri->jenjang->nama}}</h3>
+            <h3 class="box-title">Santri {{$santri->jenjang->jenis_program->nama}}</h3>
           </div>
 
-          <div class="box-body">
+          <div class="box-body table-responsive">
             @if ($santri->kelompok)
             <?php $kelompok = $santri->kelompok; ?>
           	<table class="table">
 	          	<thead>
 	              <tr>
-	              	<td><strong>Kelompok {{$kelompok->id}}</strong></td>
+	              	<td colspan="2"><strong>Kelompok {{$kelompok->id}}</strong></td>
 	              </tr>
 	          	</thead>
 	          	<tbody>
@@ -115,29 +115,29 @@
 	              	</tr>
 	              	<tr>
 		              	<td>Jadwal</td>
-		              	<td>: {{$hari[$kelompok->jadwal->hari]}}, {{date('H:i', strtotime($kelompok->jadwal->waktu))}}</td>
+		              	<td>: {{$hari[$kelompok->jadwal->hari]}}, {{$kelompok->jadwal->waktu1}}</td>
 	              	</tr>
 	              	<tr>
-		              	<td>Nomor HP/ WA</td>
-		              	<td>: {{$kelompok->jadwal->pengajar->pengguna->nomor_hp}} / {{$kelompok->jadwal->pengajar->pengguna->nomor_wa}}</td>
+		              	<td>Nomor HP, WA</td>
+		              	<td>: {{$kelompok->jadwal->pengajar->pengguna->nomor_hp}}, {{$kelompok->jadwal->pengajar->pengguna->nomor_wa}}</td>
 	              	</tr>
 	            </tbody>
           	</table>
 
-            <table class="table">
+            <table class="table table-bordered table-striped">
               <thead>
                 <tr>
-                  <th>Nomor</th>
+                  <th>No.</th>
                   <th>Nama Santri</th>
-                  <th>Nomor HP/ WA</th>
+                  <th>Nomor HP, WA</th>
                 </tr>
               </thead>
               <tbody>
                 @foreach ($kelompok->daftar_santri as $santri)
                 <tr>
-                  <td>{{$loop->iteration}}</td>
+                  <td width="5%">{{$loop->iteration}}</td>
                   <td>{{$santri->pengguna->nama_lengkap}}</td>
-                  <td>{{$santri->pengguna->nomor_hp}} / {{$santri->pengguna->nomor_wa}}</td>
+                  <td>{{$santri->pengguna->nomor_hp}}, {{$santri->pengguna->nomor_wa}}</td>
                 </tr>
                 @endforeach
                 </tr>
