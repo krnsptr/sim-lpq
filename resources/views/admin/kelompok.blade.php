5ed936edfac7e0f576438925bdefd88ef93761bf
Kurnia Saputra <krnsptr@gmail.com>
2017-09-16 08:42:14 +0700

    perbaikan tombol tutup modal halaman Kelompok

--- resources/views/admin/kelompok.blade.php
+++ resources/views/admin/kelompok.blade.php
@@ -73,7 +73,7 @@
             <!-- Modal content-->
             <div class="modal-content">
               <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal-jenjang">&times;</button>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Edit Jadwal</h4>
               </div>
               <div class="modal-body table-condensed">
@@ -115,7 +115,7 @@
                 </table>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal-jadwal">Tutup</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
               </div>
             </div>
 
